5c7f10f3c109aeaa3fecf92eabcfc8237504d9c5
rahulraghavankklm <rahulraghavankklm@gmail.com>
2014-11-04 19:06:40 +0530

    Using FIXTURES

--- app/models/todo.js
+++ app/models/todo.js
@@ -4,3 +4,25 @@ export default DS.Model.extend({
 	title: DS.attr('string'),
 	isCompleted: DS.attr('boolean')
 });
+
+Todo.reopenClass({
+	FIXTURES: [
+		{
+			id: "1",
+			title: "Ember.js App",
+			isCompleted: false
+		},
+		{
+			id: "2",
+			title: "Leaning Ember.js TodoMVC",
+			isCompleted: false
+		},
+		{
+			id: "3",
+			title: "Develop Amazing Things",
+			isCompleted: true
+		}
+	]
+});
+
+export default Todo;
