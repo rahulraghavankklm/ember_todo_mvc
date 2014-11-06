a171dc490e7ae015f613298babdde957ab195e36
rahulraghavankklm <rahulraghavankklm@gmail.com>
2014-11-06 07:04:54 +0530

    Adding Child Routes

--- app/router.js
+++ app/router.js
@@ -1,12 +1,8 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-var Router = Ember.Router.extend({
-  location: config.locationType
+export default Ember.Route.extend({
+  model: function() {
+    return this.modelFor('todos');
+  }
 });
-
-Router.map(function() {
-	this.resource('todos', {path: '/'});
-});
-
-export default Router;
