9ef55ef6d495034499ff29d4d1848a88979552af
rahulraghavankklm <rahulraghavankklm@gmail.com>
2014-11-06 06:58:35 +0530

    Deleting a Model

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -3,19 +3,26 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
 	
 	actions: {
+		
 		editTodo: function() {
 			this.set('isEditing', true);
-		}
-	},
+		},
 
-	acceptChanges: function() {
-		this.set('isEditing', false);
+		acceptChanges: function() {
+			this.set('isEditing', false);
 
-        if (Ember.isEmpty(this.get('model.title'))) {
-          this.send('removeTodo');
-        } else {
-          this.get('model').save();
-        }
+	        if (Ember.isEmpty(this.get('model.title'))) {
+	          this.send('removeTodo');
+	        } else {
+	          this.get('model').save();
+	        }
+		},
+
+		removeTodo: function() {
+			var todo = this.get('model');
+			todo.deleteRecord();
+			todo.save();
+		},
 	},
 
 	isEditing: false,
